be16db76cf45c71a69132c7d6b97445e6a45e580
Adrian Knoblauch <adrianknoblauch@me.com>
2016-10-30 20:47:36 +0100

    Assignment 19: Remove AddressBook entry

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,24 @@ require_relative '../models/address_book'
       end
     end
 
+    describe "#remove entry" do
+      it "removes an entry using the name, phone_number, and email address" do
+        book = AddressBook.new
+        book.add_entry('Adrian Knoblauch', '000.000.0000', 'adrian@knoblauch.com')
+
+        name = "Ada Lovelace"
+        phone_number = "010.012.1815"
+        email = "augusta.king@lovelace.com"
+        book.add_entry(name, phone_number, email)
+
+        expect(book.entries.size). to eq 2
+        book.remove_entry(name, phone_number, email)
+        expect(book.entries.size).to eq 1
+        expect(book.entries.first.name).to eq("Adrian Knoblauch")
+      end
+    end
+
+
    describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
@@ -36,5 +54,5 @@ require_relative '../models/address_book'
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
    end
-   
+
  end
