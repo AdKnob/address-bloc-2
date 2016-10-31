492546826872835eeea59ef19a2877d985237a66
Adrian Knoblauch <adrianknoblauch@me.com>
2016-10-31 21:30:55 +0100

    Assignment 20: View entry n, completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View Entry Number n"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -36,6 +37,10 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system "clear"
+      view_entry_n
+      main_menu
+    when 6
       puts "Good-bye!"
       exit(0)
     else
@@ -44,6 +49,21 @@ class MenuController
       main_menu
   end
 end
+
+  def view_entry_n
+    puts "Enter the number n you want to view: "
+    selection = gets.chomp.to_i
+
+    if selection < @address_book.entries.count
+      puts @address_book.entries(selection)
+      puts "Press enter to return to the main menu"
+      gets.chomp
+      system "clear"
+    else
+      puts "#{selection} is not a valid input"
+      view_entry_n
+    end
+  end
 
   def view_all_entries
     address_book.entries.each do |entry|
@@ -61,7 +81,7 @@ end
     print "Name: "
     name = gets.chomp
     print "Phone number: "
-    phone = gets.chomp
+    phone_number = gets.chomp
     print "Email: "
     email = gets.chomp
     address_book.add_entry(name, phone_number, email)
